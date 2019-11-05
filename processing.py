ecf35e180af5a0b3333ed9533ce7ce78468165d6
Nina Nguyen <ninamailan90@gmail.com>
2019-11-04 18:49:20 -0800

    GAUSSIAN BLUR WORKS!!!

--- processing.py
+++ processing.py
@@ -39,20 +39,39 @@ def count_seg(filename):
     label_arr, num_seg = ndi.label(np.invert(binary))
     return num_seg
 
-def label_segments(filename,savename=''):
+def label_segments(filename,savename='',photo=False,marker=True):
     image = io.imread(filename)
-    gray_image = color.rgb2gray(image)
-    thresh = filters.threshold_mean(gray_image)
-    binary = gray_image > thresh
-    # io.imshow(binary)
-    # io.show()
-    #io.imsave(savename+'_original.png',binary*1)
-    binary = tf.image.convert_image_dtype(binary*1, dtype=tf.uint8)  ##suppresses warning of lossy conversion
-    io.imsave(savename+'_original.png',binary)
+    image = cv2.blur(image, (10, 10)) 
+    io.imshow(image)
+#     io.imshow(image)
+    if photo:
+        arrraaayyy = np.array(image)
+        gray_image = color.rgb2gray(arrraaayyy)
+        meaaannn = np.mean(gray_image)
+        if marker:
+            binary = gray_image>(meaaannn*.65)
+        else:
+            binary = gray_image>(meaaannn*.9)
+        huh = (binary==1)*0+(binary==0)*1
+        temp = savename+'_original.jpg'
+        io.imsave(temp,np.array(huh, dtype = np.uint8 ))
+        label_arr, num_seg = ndi.label(huh)
+        segments = np.arange(1,num_seg+1)
+        plt.imshow(huh)
+        return huh,np.array(label_arr),segments,image #label_segments(temp,savename,photo=False)
+    else:
+        gray_image = color.rgb2gray(image)
+        thresh = filters.threshold_mean(gray_image)
+        binary = gray_image > thresh
+
+    io.imsave(savename+'_original.jpg',(np.array(binary*1, dtype = np.uint8 )))
+#     binary = tf.image.convert_image_dtype(binary, dtype=tf.uint8)  ##suppresses warning of lossy conversion
+#     io.imsave(savename+'_original.png',binary)
     label_arr, num_seg = ndi.label(np.invert(binary))
-    #print("number of segments", num_seg)
+    
     segments = np.arange(1,num_seg+1)
-    return binary,np.array(label_arr),segments,image
+    print(segments)
+    return binary*1,np.array(label_arr),segments,image
 
 def plot_image(label_arr):
     plt.subplots(ncols=1, nrows=1, figsize=(8, 8))
@@ -86,6 +105,7 @@ def convert_inkml(directory, out_folder):
     for i in f.readlines():
         ink_files.append(i[:-1])
     f.close()    
+
     for filename in ink_files:
         outfile = out_folder +filename[len(directory):-6]+'.png'
         print(filename,outfile)
@@ -96,8 +116,7 @@ def convert_inkml(directory, out_folder):
 def crop_image(segment,label_arr,binary_arr,ax=None,plot=False,model=None,direc='export/',svc=False,tf=False):
     nrows = 28
     ncolumns = 28
-    found = label_arr == segment    
-    #plt.imshow(found)
+    found = label_arr == segment   
     x,y = np.where(found)
     xmin,xmax,ymin,ymax = np.min(x),np.max(x),np.min(y),np.max(y)
     xlen,ylen = found[xmin:xmax,ymin:ymax].shape
@@ -117,9 +136,15 @@ def crop_image(segment,label_arr,binary_arr,ax=None,plot=False,model=None,direc=
         elif ylen>xlen: xmax+=diff
 
     prediction=None
-    digit = np.invert(binary_arr[xmin:xmax,ymin:ymax])
-    digit = np.pad(digit,int(len(digit)*.2))
-    xlen,ylen = digit.shape
+#     digit = np.invert(binary_arr[xmin:xmax,ymin:ymax])
+    digit = binary_arr[xmin:xmax,ymin:ymax]
+    digit = np.pad(digit,int(len(digit)*.2),mode= 'constant', constant_values=(1,1))  
+#     io.imshow(digit)
+#     print(digit)
+    try:
+        xlen,ylen = digit.shape
+    except:
+        return
     if ylen==0:
         if plot: ax.set_visible(False)
         return None,[ymin,prediction],xmin,digit  
@@ -152,26 +177,34 @@ def crop_image(segment,label_arr,binary_arr,ax=None,plot=False,model=None,direc=
         else:
             plt.imshow(digit,cmap='Greys_r')
             plt.title('Segment #'+str(segment)+predicted,fontsize=15)
-
     return img,[ymin,prediction],digit
     
 
 
-def process_image(filename,dirname,fitted_clf,plot=False,svc=False,tf=False):
-    binary_arr,label_arr, segments,orig = label_segments(filename,dirname)
+def process_image(filename,dirname,fitted_clf,plot=False,svc=False,tf=False,photo=False):
+    binary_arr,label_arr, segments,orig = label_segments(filename,dirname,photo)
     # plot_numbered_image(label_arr,dirname)
     temp = 'tempimgs/'
     predicted=[]    
     if plot:
         fig,axes = plt.subplots(len(segments),1,figsize=(5,len(segments)*5))
         for seg,ax in list(zip(segments,axes.flatten())):
-
-            predicted.append(crop_image(seg,label_arr,orig,ax=ax,plot=True,model=fitted_clf,direc=temp,svc=svc,tf=tf)[1])
+            try: 
+                if crop_image(seg,label_arr,orig,model=fitted_clf,direc=temp,svc=False,tf=True)==None:
+                    predicted.append(None)
+            except:
+                predicted.append(crop_image(seg,label_arr,orig,ax=ax,plot=True,model=fitted_clf,direc=temp,svc=svc,tf=tf)[1])
         fig.savefig(dirname+'_predictions.png')
     else:
         for seg in segments:
             if tf:
-                predicted.append(crop_image(seg,label_arr,orig,model=fitted_clf,direc=temp,svc=False,tf=True)[1])            
+                try: 
+                    if crop_image(seg,label_arr,orig,model=fitted_clf,direc=temp,svc=False,tf=True)==None:
+                        finding='%'
+                except:
+                    finding = crop_image(seg,label_arr,orig,model=fitted_clf,direc=temp,svc=False,tf=True)[1]
+                if finding!= None:
+                    predicted.append(finding)            
             elif svc:
                 predicted.append(crop_image(seg,label_arr,orig,model=fitted_clf,direc=temp,svc=True,tf=False)[1])
             else:
@@ -190,8 +223,16 @@ def process_image(filename,dirname,fitted_clf,plot=False,svc=False,tf=False):
             except:
                 pred.pop()
     result = np.array(pred)
-    edited = np.where(result==None, 1, result)
-    return edited
+    result = np.where(result==None, 1, result)
+    if result.shape!=(4,):
+        result = list(result)
+        try:
+            result.sort()
+            result = np.array([p[0] for p in result]   )
+        except:
+            pass
+#     edited = np.where(result==None, 1, result)
+    return result
 
 
 def setdiff(x):
@@ -212,7 +253,7 @@ def main(prepared=0,svc=False,fitted_clf=None,tf=False):
         matname = 'tf22/output__'+str(index)
         print(image)
         predictions.append(process_image(image,matname,fitted_clf,svc=svc,tf=tf))
-
+    
     predictions=np.array(predictions)
     results['predict'] = predictions
     results['correct']=results.apply(setdiff,axis=1)
